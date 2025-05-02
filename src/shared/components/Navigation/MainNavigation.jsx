28839fd67d05a143c370d9cfb5efb7e256194de9
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-02 21:45:14 +0800

    feat: added side drawer for mobile

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -3,22 +3,30 @@ import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import './MainNavigation.css';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 
 function MainNavigation() {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className="main-navigation__title">
-        <Link>PlaceBook</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className="main-navigation__title">
+          <Link>PlaceBook</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 }
 
