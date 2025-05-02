4036b5809cb37de195125e1c9c8db2de62fa889e
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-02 16:59:12 +0800

    feat: added UsersList Page/Component

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
+const USERS = [
+  {
+    id: 'u1',
+    name: 'Aiu',
+    image: 'sample image url',
+    places: 3,
+  },
+];
 
 function Users() {
-  return <div>Users</div>;
+  return <UsersList items={USERS} />;
 }
 
 export default Users;
