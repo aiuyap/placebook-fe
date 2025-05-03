9d255b3590dc2c0d5744d3e7d13dfe96bb12413a
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-03 14:53:57 +0800

    feat: added custom button component with styling

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './PlaceItem.css';
 import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
 
 function PlaceItem(props) {
   return (
@@ -15,9 +16,9 @@ function PlaceItem(props) {
           <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${props.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
