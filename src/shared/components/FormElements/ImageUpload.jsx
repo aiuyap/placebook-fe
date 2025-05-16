5189020866aa1d664ce40b406ebd90ebe130daa8
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-05-16 08:04:39 +0800

    feat: created ImageUpload component

--- src/shared/components/FormElements/ImageUpload.jsx
+++ src/shared/components/FormElements/ImageUpload.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import Button from '../FormElements/Button';
 import './ImageUpload.css';
 
-function ImageUpload() {
-  return <div>ImageUpload</div>;
+function ImageUpload(props) {
+  const filePickerRef = useRef();
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  const pickedHandler = (e) => {
+    console.log(e.target);
+  };
+
+  return (
+    <div className="form-control">
+      <input
+        id={props.id}
+        type="file"
+        style={{ display: 'none' }}
+        accept=".jpg,.png,.jpeg"
+        ref={filePickerRef}
+        onChange={pickedHandler}
+      />
+      <div className={`image-upload ${props.center && 'center'}`}>
+        <div className="image-upload__preview">
+          <img src="" alt="Preview" />
+        </div>
+        <Button type="button" onClick={pickImageHandler}>
+          PICK IMAGE
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ImageUpload;
--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -13,6 +13,7 @@ import { AuthContext } from '../../shared/context/auth-context';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { useHttpClient } from '../../shared/hooks/http-hook';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 
 function Auth() {
   const auth = useContext(AuthContext);
@@ -103,6 +104,7 @@ function Auth() {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && <ImageUpload id="image" center />}
           <Input
             id="email"
             type="email"
